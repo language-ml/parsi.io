4cc99ac7646585b5bd45cbe3eb5b1041e0bc16bc
Admin <admin@language.ml>
2022-02-17 03:39:06 +0330

    first commit

---
+++ parsi_io/modules/address_extractions.py
@@ -0,0 +1,9 @@
+
+
+class AddressExtraction(object):
+    def __init__(self):
+        # model initialization
+        self.model = str
+
+    def run(self, text):
+        return self.model(text)
---
+++ parsi_io/modules/template.py
@@ -0,0 +1,9 @@
+
+
+class Model(object):
+    def __init__(self):
+        # model initialization
+        self.model = str
+
+    def run(self, text):
+        return self.model(text)
